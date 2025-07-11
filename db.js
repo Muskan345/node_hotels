0f64ddff1069f1b7c0eb44b3f9e93460c47cc3af
Muskan Thakur <95755566+Muskan345@users.noreply.github.com>
2025-07-11 16:40:21 +0530

    remove duplicate data

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 //Define the MongoDB connnection URL
-//const mongoURL = "mongodb://localhost:27017/hotels";
-const mongoURL = process.env.MONGODB_URL_LOCAL; //
 const mongoURL = process.env.MONGODB_URL;
 
 //Set Up the MongoDB Connection
